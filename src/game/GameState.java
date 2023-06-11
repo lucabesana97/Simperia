fe0a1bd12640729d7b43862e2df9219bd6d3fb09
Sophia Soares <sophia.soares.silva@gmail.com>
2023-06-11 19:59:51 +0200

    Changed home panel and inventory panel

---
+++ src/game/GameState.java
@@ -0,0 +1,10 @@
+package game;
+
+public enum GameState {
+    HOME, // Home panel
+    PLAYING, // Playing the game
+    PAUSED, // Pause panel
+    INVENTORY, // Inventory panel
+    GAMEOVER, // Dead
+    WIN // Won the game
+}
