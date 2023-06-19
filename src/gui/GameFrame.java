e3440c002d3fd18bc48150fd51da56d5e6350458
Sophia Soares <sophia.soares.silva@gmail.com>
2023-06-19 20:56:40 +0200

    Removed testing HEIGHT value from game frame

--- src/gui/GameFrame.java
+++ src/gui/GameFrame.java
@@ -11,9 +11,7 @@ public class GameFrame extends JFrame{
 
 	//Dimensions of the window
 	public static final int WIDTH = 1024;
-	//public static final int HEIGHT = 768;
-
-	public static final int HEIGHT = 668; // TODO: change back to 768
+	public static final int HEIGHT = 768;
 	public static final int TILE_SIZE = 64;
 
 
